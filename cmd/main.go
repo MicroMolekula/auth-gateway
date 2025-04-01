d8b1cf3d9e1498f0495736a7aa70b4c2b43968a5
Красиков Иван <krasikov@intaro.email>
2025-04-01 22:31:12 +0300

    init project

---
+++ cmd/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	data := map[string]string{
+		"name":    "Auth Gateway",
+		"version": "0.0",
+	}
+	response, err := json.Marshal(data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	if _, err = w.Write(response); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func main() {
+	http.HandleFunc("/", helloHandler)
+	fmt.Println("Сервер запущен на порту 8000")
+	if err := http.ListenAndServe(":8000", nil); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/MicroMolekula/auth-gateway
+
+go 1.23.1
