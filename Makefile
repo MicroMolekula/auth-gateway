3080b991badc027fcc1666ca07b22876008d574c
MicroMolekula <ivan1.krasikov.2003@gmail.com>
2025-04-01 23:05:34 +0300

    add Test Controller

--- Makefile
+++ Makefile
@@ -2,3 +2,6 @@ build:
 	docker build -t not-five/auth-gateway .
 run:
 	docker run -d -p 127.0.0.1:8000:8000 --name=auth-gateway not-five/auth-gateway
+stop:
+	docker stop auth-gateway
+	docker remove auth-gateway
--- cmd/main.go
+++ cmd/main.go
@@ -21,8 +21,22 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func testHandler(w http.ResponseWriter, r *http.Request) {
+	data := map[string]string{
+		"name": "Test endpoint",
+	}
+	response, err := json.Marshal(data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	if _, err = w.Write(response); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
 func main() {
 	http.HandleFunc("/", helloHandler)
+	http.HandleFunc("/test", testHandler)
 	fmt.Println("Сервер запущен на порту 8000")
 	if err := http.ListenAndServe(":8000", nil); err != nil {
 		log.Fatal(err)
